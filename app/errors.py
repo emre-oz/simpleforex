f7dd7f69faeeef770c006af16625b22444213ee2
emre-oz <erturk.emre98@gmail.com>
2019-04-23 18:08:18 +0200

    error handling fix

--- app/errors.py
+++ app/errors.py
@@ -4,3 +4,7 @@ from app import app
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template('500.html'), 500
